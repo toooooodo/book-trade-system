1c33725f563c55c0d4bb8f165ffb9d294042ab0f
toooooodo <1042178105@qq.com>
2019-06-08 00:00:50 +0800

    登录重定向有bug

--- app/urls.py
+++ app/urls.py
@@ -4,7 +4,10 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path(r'', views.login),
+    path(r'', views.index),
+    url(r'^index/$', views.index),
+    url(r'^login/$', views.login),
+    url(r'^dologin/$', views.dologin),
     url(r'^register/$', views.register),
     url(r'^doregister/$', views.doregister),
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -12,33 +12,58 @@ def login(request):
     return render(request, 'app/login.html')
 
 
+@csrf_exempt
+def dologin(request):
+    if request.session.get('is_login', None):
+        return redirect('app/index.html')
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        res = User.objects.filter(Q(username__exact=username) & Q(password__exact=password))
+        if len(res) == 0:
+            # 用户不存在
+            return JsonResponse({'status': '0'})
+        else:
+            # 验证成功
+            request.session['is_login'] = True
+            request.session['user_id'] = res[0].id
+            request.session['user_name'] = res[0].username
+            return redirect('/index/')
+
+
 def register(request):
     return render(request, 'app/register.html')
 
 
 @csrf_exempt
 def doregister(request):
-    username = request.POST.get('username')
-    email = request.POST.get('email')
-    password = request.POST.get('password')
-    print(username, email, password)
-    # res = User.objects.filter(username=username, email=email)
-    # res = User.objects.filter(Q(username__exact=username) | Q(email__exact=email))
-    res_user = User.objects.filter(username=username)
-    res_email = User.objects.filter(email=email)
-    if len(res_user) == 0 and len(res_email) == 0:
-        u = User()
-        u.username = username
-        u.password = password
-        u.email = email
-        u.save()
-        return_dict = {'status': '0'}
-        return JsonResponse(return_dict)
-    elif len(res_user) == 0:
-        # 邮箱已被注册
-        return_dict = {'status': '1'}
-        return JsonResponse(return_dict)
-    else:
-        # 用户名已被注册
-        return_dict = {'status': '2'}
-        return JsonResponse(return_dict)
+    if request.method == "POST":
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        print(username, email, password)
+        # res = User.objects.filter(username=username, email=email)
+        # res = User.objects.filter(Q(username__exact=username) | Q(email__exact=email))
+        res_user = User.objects.filter(username=username)
+        res_email = User.objects.filter(email=email)
+        if len(res_user) == 0 and len(res_email) == 0:
+            u = User()
+            u.username = username
+            u.password = password
+            u.email = email
+            u.save()
+            return_dict = {'status': '0'}
+            return JsonResponse(return_dict)
+        elif len(res_user) == 0:
+            # 邮箱已被注册
+            return_dict = {'status': '1'}
+            return JsonResponse(return_dict)
+        else:
+            # 用户名已被注册
+            return_dict = {'status': '2'}
+            return JsonResponse(return_dict)
+
+
+def index(request):
+    return render(request, 'app/index.html')
